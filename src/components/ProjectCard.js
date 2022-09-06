e52d853753b47b28d269aee132920a59f666f1d5
Brandon-Chan03 <Brandon.chan0216@gmail.com>
2022-09-05 20:44:15 -0500

    fixed some styling issues and made project card a button with link to project

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -3,15 +3,16 @@ import { Col } from "react-bootstrap";
 export const ProjectCard = ({ title, description, imgUrl, UrlLink }) => {
   return (
     <Col size={12} sm={6} md={4}>
-      <div className="proj-imgbx">
-        <img src={imgUrl} alt=""/>
-        <div className="proj-txtx">
-          <h4>{title}</h4>
-          <span>{description}</span>
-        </div>
-      </div>
       <div className='project-link'>
-        <a href={UrlLink} target="_blank" rel="noreferrer">{`Link to ${title}`}</a>
+        <a href={UrlLink} target="_blank" rel="noreferrer">
+          <div className="proj-imgbx">
+            <img src={imgUrl} alt="" />
+            <div className="proj-txtx">
+              <h4>{title}</h4>
+              <span>{description}</span>
+            </div>
+          </div>
+        </a>
       </div>
     </Col>
   );
